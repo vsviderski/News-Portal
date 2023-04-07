8938789c2725199aebb75fdb5954df752afaf9d3
V.Sviderski <svidvlad15@gmail.com>
2023-04-07 21:12:00 +0300

    Removed Counter.tsx

--- src/components/Counter.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import classNames from './Counter.module.scss';
-
-const Counter: FC = () => {
-  return <div className={classNames.text}>TEST</div>;
-};
-
-export default Counter;
