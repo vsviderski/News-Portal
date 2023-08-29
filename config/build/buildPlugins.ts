d9e1d9e61b6650e1d1e2cd1623c03bfab4673d55
V.Sviderski <svidvlad15@gmail.com>
2023-08-29 21:58:21 +0300

    Fixed buildPlugins.ts

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,17 +7,24 @@ import { BuildOptions } from './types/config';
 export const buildPlugins = ({
     paths,
     isDev,
-}: BuildOptions): WebpackPluginInstance[] => [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-        template: paths.html,
-    }),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
-];
+}: BuildOptions): WebpackPluginInstance[] => {
+    const plugins = [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+    ];
+
+    if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+
+    return plugins;
+};
