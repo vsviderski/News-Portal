e5e10204a5f6f65d839fd89fc0e74f6be0e82edf
V.Sviderski <svidvlad15@gmail.com>
2023-10-06 18:34:59 +0300

    Added tests for loginSlice

--- src/features/AuthByUserName/model/slice/loginSlice.test.ts
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -1,6 +1,24 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { ILoginSchema } from "../types/LoginSchema";
+import { loginActions, loginFormReducer } from "./loginSlice";
+import { loginByUserName } from "../services/loginByUserName/loginByUserName";
+
 describe('loginSlice', () => {
-    test('decrement', () => {
-        // const state: DeepPartial<ICounterSchema> = { value: 10 };
-        // expect(counterReducer(state as ICounterSchema, counterActions.decrement())).toEqual({ value: 9 });
+    test('set userName', () => {
+        const state: DeepPartial<ILoginSchema> = { username: '' };
+        expect(loginFormReducer(state as ILoginSchema, loginActions.userName('test'))).toEqual({ username: 'test' });
+    });
+
+    test('set password', () => {
+        const state: DeepPartial<ILoginSchema> = { password: '' };
+        expect(loginFormReducer(state as ILoginSchema, loginActions.password('test')))
+            .toEqual({ password: 'test' });
+    });
+
+    test('check isLoading', () => {
+        const state: DeepPartial<ILoginSchema> = { isLoading: false };
+        expect(loginFormReducer(state as ILoginSchema, loginByUserName.pending)).toEqual({ isLoading: true });
+        expect(loginFormReducer(state as ILoginSchema, loginByUserName.fulfilled)).toEqual({ isLoading: false });
+        expect(loginFormReducer(state as ILoginSchema, loginByUserName.rejected)).toEqual({ isLoading: false });
     });
 });
